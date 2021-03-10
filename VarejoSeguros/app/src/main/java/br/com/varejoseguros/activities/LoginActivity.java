4af1931db0b80d41f402f17931ddb341b7900f69
Victor Fernando de Oliveira Sampaio <victorsampaio@users.noreply.github.com>
2021-03-10 06:40:47 -0300

    Start Commit LOGIN #1

---
+++ VarejoSeguros/app/src/main/java/br/com/varejoseguros/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.varejoseguros.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.varejoseguros.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
